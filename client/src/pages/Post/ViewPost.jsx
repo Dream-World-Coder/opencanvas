75227fd003f823addcdac80eeb9a7eecf2afeff0
Dream-World-Coder <dreamy.star.368@gmail.com>
2025-03-05 17:28:21 +0530

    files for post view & edit

---
+++ client/src/pages/Post/ViewPost.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import { MarkdownPreview } from "../CreatePosts/Writing/WritingComponents";
+import Header from "../../components/Header/Header";
+
+// Its the public view --> add options like link share
+
+export default function ViewPost(postId) {
+    return <></>;
+}
