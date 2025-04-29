2cdceacc053a2384063caf9b284fef279b46ae83
Dream-World-Coder <dreamy.star.368@gmail.com>
2025-04-29 17:54:06 +0530

    updated private post view

--- client/src/pages/Post/PrivatePostView.jsx
+++ client/src/pages/Post/PrivatePostView.jsx
@@ -7,20 +7,13 @@ import { MarkdownPreview } from "../CreatePosts/Writing/WritingComponents";
 import { useDataService } from "../../services/dataService";
 import { useDarkMode } from "../../components/Hooks/darkMode";
 import { toast } from "sonner";
-import {
-    PostHelmet,
-    LoadingPost,
-    NotPost,
-    LeftSidebar,
-    RightSidebar,
-} from "./components";
+import { LoadingPost, NotPost, LeftSidebar, RightSidebar } from "./components";
 
 const PrivatePostView = ({ isArticle = true }) => {
     const location = useLocation();
     const { postId } = useParams();
     const { getPostByIdSecured } = useDataService();
     const isDark = useDarkMode();
-    // const focusMode = true;
     const [post, setPost] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -50,51 +43,51 @@ const PrivatePostView = ({ isArticle = true }) => {
     if (!post) return <NotPost />;
 
     return (
-        <>
-            <PostHelmet post={post} />
-            <div className="w-full h-full grid place-items-center bg-white dark:bg-[#111] overflow-x-hidden pt-16">
-                <Header
-                    noBlur={true}
-                    ballClr={"text-gray-300"}
-                    exclude={["/about", "/contact", "/photo-gallery"]}
-                    abs={true}
-                />
-                <div className="flex flex-col md:flex-row min-h-screen max-w-screen-xl mx-auto bg-white dark:bg-[#111] text-gray-900 dark:text-gray-100">
-                    <LeftSidebar focusMode={true} isArticle={isArticle} />
-                    <main
-                        className={`flex-1 p-4 md:p-6 lg:p-8 min-h-screen max-w-3xl`}
-                    >
-                        <div className="prose dark:prose-invert max-w-none pt-4 mb-16">
-                            <MarkdownPreview
-                                title={post.title}
-                                content={post.content}
-                                thumbnailUrl={post.thumbnailUrl}
-                                isDark={isDark}
-                                darkBg="bg-[#111]"
-                                textAlignment={
-                                    post.type !== "poem" ? "left" : "center"
-                                }
-                                insidePost={true}
-                                artType={post.type}
-                            />
-                            <div className="flex flex-wrap gap-2 mb-6">
-                                {post.tags.map((tag, index) => (
-                                    <span
-                                        key={index}
-                                        className="text-xs px-2 py-1 rounded-full bg-gray-100 dark:bg-[#222] text-gray-700 dark:text-gray-300"
-                                    >
-                                        {tag}
-                                    </span>
-                                ))}
-                            </div>
+        <div className="w-full h-full grid place-items-center bg-white dark:bg-[#111] overflow-x-hidden pt-16">
+            <Header
+                noBlur={true}
+                ballClr={"text-gray-300"}
+                exclude={["/about", "/contact", "/photo-gallery"]}
+                abs={true}
+            />
+            <div className="flex flex-col md:flex-row min-h-screen max-w-screen-xl mx-auto bg-white dark:bg-[#111] text-gray-900 dark:text-gray-100">
+                <LeftSidebar focusMode={true} isArticle={isArticle} />
+                <main
+                    className={`flex-1 p-4 md:p-6 lg:p-8 min-h-screen max-w-3xl`}
+                >
+                    <div className="prose dark:prose-invert max-w-none pt-4 mb-16">
+                        <MarkdownPreview
+                            title={post.title}
+                            content={post.content}
+                            thumbnailUrl={post.thumbnailUrl}
+                            isDark={isDark}
+                            darkBg="bg-[#111]"
+                            textAlignment={
+                                post.type !== "poem" ? "left" : "center"
+                            }
+                            insidePost={true}
+                            artType={post.type}
+                        />
+                        <div className="flex flex-wrap gap-2 mb-6">
+                            {post.tags.map((tag, index) => (
+                                <span
+                                    key={index}
+                                    className="text-xs px-2 py-1 rounded-full bg-gray-100 dark:bg-[#222] text-gray-700 dark:text-gray-300"
+                                >
+                                    {tag}
+                                </span>
+                            ))}
                         </div>
-                    </main>
-                    <RightSidebar />
-                </div>
-
-                <Footer />
+                    </div>
+                </main>
+                <RightSidebar
+                    content={post.content}
+                    isArticle={post.type.toLowerCase() === "article"}
+                />
             </div>
-        </>
+
+            <Footer />
+        </div>
     );
 };
 
